e3017bb99c544487b865b21807e046b2cd3fa598
Sara Soueidan <saraesoueidan@gmail.com>
2021-11-13 18:18:34 -0500

    set up habit route

--- src/route/habit.route.js
+++ src/route/habit.route.js
@@ -4,7 +4,7 @@ import { Router } from 'express';
 import bodyParser from 'body-parser';
 
 import basicAuth from '../lib/basic-auth-middleware.js'
-import User from '../model/user.js';
+import Habit from '../model/habit.js';
 
 const authRouter = module.exports = new Router();
 
